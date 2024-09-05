d00d7ca54eea25a5f03a186177ba6f53892b1c27
Hasanah977 <hasanabakri911@gmail.com>
2024-09-05 18:10:56 +0700

    Add files via upload

---
+++ Days4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days4 {
+    public static void main(String[] args) {
+        System.out.print("hii");
+        System.out.print("sisa 96 hari lagi");
+        System.out.println();
+        System.out.printf("nama: hasanah");
+        System.out.printf("age: 18");
+        
+    }
+    
+}
