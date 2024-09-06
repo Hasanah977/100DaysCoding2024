68d844b03970d386813daaa6bbf074905b9eceaf
Hasanah977 <hasanabakri911@gmail.com>
2024-09-06 18:07:24 +0700

    Add files via upload

---
+++ Days5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days5 {
+    public static void main(String[] args) {
+       //ini adalah komentar satu baris
+        String a = "Indonesia generasi emas";
+        int b = 2045;  // ini adalah komentar di akhir kode  
+        /* ini adalah komentar multi baris untuk menjelaskan lebih detail
+        terkait fungsi tersebut.
+        */
+        System.out.printf("%s%n", a);
+        System.out.printf("%d%n", b);
+    
+        System.out.print ("indonesia generasi emas");
+        System.out.print ("2045");
+    } 
+}
