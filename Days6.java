3d3278657a700fca702fc7b05d5052c6df7dff9f
Hasanah977 <hasanabakri911@gmail.com>
2024-09-07 19:20:53 +0700

    Add files via upload

---
+++ Days6.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days6 {
+    public static void main(String[] args) {
+        int age = 19;
+        double livingExpenses = 10.00;
+        String greeting = "namanya juga hidup";
+        
+        System.out.println("age");
+        System.out.println("living Expenses");
+        System.out.println("greeting");
+    }
+    
+}
