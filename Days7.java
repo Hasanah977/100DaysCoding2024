9c71cc0017834dcfcc881d4e4675f5215d2265aa
Hasanah977 <hasanabakri911@gmail.com>
2024-09-08 18:01:53 +0700

    Add files via upload

---
+++ Days7.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days7 {
+    public static void main(String[] args) {
+        var k = 2045;
+    }
+}
