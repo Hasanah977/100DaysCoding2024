15bcaaef4df295fb648e3cfa7c25277316ea8063
Hasanah977 <hasanabakri911@gmail.com>
2024-09-11 20:22:42 +0700

    Delete Days10.java

--- Days10.java
+++
@@ -1,9 +0,0 @@
-
-public class Days10 {
-    public static void main(String[] args) {
-        byte x = -127;
-        byte y = 127;
-        System.out.println("nilai x: " + x);
-        System.out.println("nilai y: " + y);
-    }
-}
