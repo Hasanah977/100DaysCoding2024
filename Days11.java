320a45add73996a8cc978c15e0de25ee2886bc00
Hasanah977 <hasanabakri911@gmail.com>
2024-09-12 19:00:40 +0700

    Add files via upload

---
+++ Days11.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days11 {
+    public static void main(String[] args) {
+        short rinjani = 3726;
+        System.out.println("rinjani" + rinjani + "mdpl");
+    }
+}
