97c6a858b2afc2ce425ca2a32544158ec871b129
Hasanah977 <hasanabakri911@gmail.com>
2024-09-13 18:51:47 +0700

    Add files via upload

---
+++ Days12.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days12 {
+    public static void main(String[] args) {
+        int E = 20;
+        int F = 40;
+        System.out.println("E: " + E);
+        System.out.println("F: " + F);
+    }
+    
+}
