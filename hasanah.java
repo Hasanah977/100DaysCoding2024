06433bd061d9450579795e45d14e58b871b6d3b4
Hasanah977 <hasanabakri911@gmail.com>
2024-09-14 20:48:05 +0700

    Add files via upload

---
+++ hasanah.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class hasanah {
+
+    public static void main(String[] args) {
+        System.out.println("hasanah");
+        String nama = "hasanah";
+        String alamat = "BTN Linomaloga";
+        int angkatan = 2024;
+        System.out.println("nama \t:" + nama);
+        System.out.println("alamat\t:" + alamat);
+        System.out.println("angkatan\t:" + angkatan);
+         
+    }
+}
