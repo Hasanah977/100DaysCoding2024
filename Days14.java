6dce08ca377dc2b6c3547b09c6b2826221c21659
Hasanah977 <hasanabakri911@gmail.com>
2024-09-15 20:57:11 +0700

    Add files via upload

---
+++ Days14.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days14 {
+    public static void main(String[] args) {
+        long Jarak_Indnesia_Mekkah = 839300000L;
+     System.out.println("Jarak_Indnesia_Mekkah: " +Jarak_Indnesia_Mekkah+ "km");
+    }
+    
+}
