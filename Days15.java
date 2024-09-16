58c76f9c7fee7c57f603f370346a031fea8259e9
Hasanah977 <hasanabakri911@gmail.com>
2024-09-16 18:15:38 +0700

    Add files via upload

---
+++ Days15.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days15 {
+    public static void main(String[] args) {
+        double Utang_Indonesia = 8353.02 ;
+       System.out.println("Utang_Indonesia: " + Utang_Indonesia + "triliun"); 
+    }
+}
