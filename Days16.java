3cbe6f42af48e9ab026c50ccf8fc8236391e225e
Hasanah977 <hasanabakri911@gmail.com>
2024-09-17 09:06:20 +0700

    Add files via upload

---
+++ Days16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days16 {
+    public static void main(String[] args) {
+        float UKT = 2.50000f;
+        System.out.println("UKT: " + UKT + "JUTA");
+    }
+    
+}
