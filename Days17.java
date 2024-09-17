97241ae29933469297894620431c7084178c6006
Hasanah977 <hasanabakri911@gmail.com>
2024-09-18 06:28:41 +0700

    Add files via upload

---
+++ Days17.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days17 {
+    public static void main(String[] args) {
+        char inisial  = 'h';
+        System.out.println(inisial);
+    }
+    
+}
