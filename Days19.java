63f6e79c62f9ca8fb0c049094b35c9fec97b339d
Hasanah977 <hasanabakri911@gmail.com>
2024-09-20 15:24:00 +0700

    Add files via upload

---
+++ Days19.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days19 {
+    public static void main(String[] args) {
+        String Nama = "Hasanah";
+        String Fakultas = "Teknik";
+        String Prodi = "Sistem Informasi";
+        String Gabung = Nama + " " + Fakultas + " " + Prodi;
+        System.out.println(Gabung);
+    }
+    
+}
