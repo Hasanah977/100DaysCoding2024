ca1848666bfbe1a259e5179b90b4d2632690a4eb
Hasanah977 <hasanabakri911@gmail.com>
2024-09-22 16:27:18 +0700

    Add files via upload

---
+++ Days21.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days21 {
+    public static void main(String[] args) {
+        String nama = "prezekc";
+        short umur = 17;
+        System.out.println("nama: " + nama);
+        System.out.println("umur: " + umur);
+    }
+    
+}
