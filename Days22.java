e7313d27a4384adb5fdb2bd917b6faf22cf77b4a
Hasanah977 <hasanabakri911@gmail.com>
2024-09-23 17:46:01 +0700

    Add files via upload

---
+++ Days22.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days22 {
+    public static void main(String[] args) {
+        double UKT = 2.500000;
+        Integer SelamaKuliah = 8;
+        double Hasil = UKT * SelamaKuliah;
+        
+        System.out.println("Hasil: " + Hasil);
+    }
+    
+}
