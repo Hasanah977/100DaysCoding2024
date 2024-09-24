00f16dd63f8e05b5ef37079e6228ed2f6b0db317
Hasanah977 <hasanabakri911@gmail.com>
2024-09-24 17:16:52 +0700

    Add files via upload

---
+++ Days23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days23 {
+    public static void main(String[] args) {
+        long speed = (long) (153.12000 * 10000);
+        System.out.println("kekayaan: " + speed);
+    }
+    
+}
