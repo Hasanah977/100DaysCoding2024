b27e789afd54cb0a1d83456890c8e733b4a0ad75
Hasanah977 <hasanabakri911@gmail.com>
2024-09-25 15:21:58 +0700

    Add files via upload

---
+++ Days24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days24 {
+    public static void main(String[] args) {
+        String nama = "hasanah";
+        float TB = 144.9f;
+        float BB = 51.60f;
+        System.out.println("nama: " + nama );
+        System.out.println("TB: " + TB);
+        System.out.println("BB: " + BB);
+    }
+    
+}
