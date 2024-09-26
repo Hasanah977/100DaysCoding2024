e7b9839dfeacb3babb5eeab0d1aadc292c0affbb
Hasanah977 <hasanabakri911@gmail.com>
2024-09-26 18:03:15 +0700

    Add files via upload

---
+++ Days25.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days25 {
+    public static void main(String[] args) {
+        double a = 12.5;
+        double b = 14.5;
+        double hasil = a + b;
+        System.out.println(hasil);
+    }
+    
+}
