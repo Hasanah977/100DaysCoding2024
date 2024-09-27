8ec0416ccf3d3ad12727addb9a637fcd2a699044
Hasanah977 <hasanabakri911@gmail.com>
2024-09-27 20:35:36 +0700

    Add files via upload

---
+++ Days26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days26 {
+    public static void main(String[] args) {
+        char Gender = 'p';
+        System.out.println("Gender: " + Gender);
+    }
+    
+}
