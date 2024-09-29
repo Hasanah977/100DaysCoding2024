267625ff913c26ee7f96b96c12d553d9c7d44b98
Hasanah977 <hasanabakri911@gmail.com>
2024-09-29 17:39:55 +0700

    Add files via upload

---
+++ Days28.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days28 {
+    public static void main(String[] args) {
+         String name = "prezekc";
+        String address = "wakanda";
+        boolean isActive = true;
+
+        System.out.println("Nama: " + name);
+        System.out.println("Alamat: " + address);
+        System.out.println("Status Aktif: " + isActive);
+    }
+    
+}
