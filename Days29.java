6e26731025ccd7e3558465d4185517c4e2b53b53
Hasanah977 <hasanabakri911@gmail.com>
2024-09-30 19:04:57 +0700

    Add files via upload

---
+++ Days29.java
@@ -0,0 +1,19 @@
+
+import java.math.BigInteger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days29 {
+    public static void main(String[] args) {
+        BigInteger nomorhp = new BigInteger("911234567");
+        System.out.println("Nomor HP: " + nomorhp);
+    }
+    
+}
