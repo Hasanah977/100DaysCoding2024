435e2825b249de66685aef3cc21f5d011f399289
Hasanah977 <hasanabakri911@gmail.com>
2024-10-01 18:12:04 +0700

    Add files via upload

---
+++ Days30.java
@@ -0,0 +1,19 @@
+
+import java.math.BigDecimal;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days30 {
+    public static void main(String[] args) {
+        BigDecimal KekayaanElonMask = new BigDecimal ("999.999999");
+        System.out.printf("KekayaanElonMask %f", KekayaanElonMask );
+    }
+    
+}
