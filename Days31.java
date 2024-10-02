cfa86e18c7ede5d1b79daaaef9dc83dcfcea583b
Hasanah977 <hasanabakri911@gmail.com>
2024-10-02 15:43:24 +0700

    Add files via upload

---
+++ Days31.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days31 {
+    public static void main(String[] args) {
+         String a = "234";  // Ini adalah string
+         int intNumber = Integer.parseInt(a);  // Mengubah string menjadi angka
+         System.out.println(intNumber); // Output: 123 (angka)
+
+    }
+    
+}
