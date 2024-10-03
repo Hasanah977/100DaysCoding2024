efa97e5be15b85127302ffb32b0b8d73489b731b
Hasanah977 <hasanabakri911@gmail.com>
2024-10-03 15:44:40 +0700

    Add files via upload

---
+++ Days32.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days32 {
+    public static void main(String[] args) {
+       int drakor = 2521;
+      String str = String.valueOf(drakor);
+        System.out.println("nilai String : " + str);
+    }
+}
