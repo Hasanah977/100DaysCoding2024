b95f1ed54aacf2183069d2388bbf98a6f3b6e470
Hasanah977 <hasanabakri911@gmail.com>
2024-10-05 19:55:45 +0700

    Add files via upload

---
+++ Days34.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("angka1: ");
+        
+        int angka1 = sc.nextInt();
+         System.out.println ("angka2: ");
+         int angka2 = sc.nextInt();
+        
+         
+         System.out.println(angka1 + angka2);
+         
+    }
+    
+}
