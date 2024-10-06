b8dc89bc5b11389c6418e4146d394cc1c2842ba9
Hasanah977 <hasanabakri911@gmail.com>
2024-10-06 18:35:57 +0700

    Add files via upload

---
+++ Days35.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days35 {
+    public static void main(String[] args) {
+        int ayam = 20;
+        int bebek = 30;
+        int jumlahhewan = ayam + bebek;
+        
+        System.out.println("jumlah hewan: " + jumlahhewan);
+        
+        int hewanmati = 10;
+        
+        System.out.println("sisa hewan: " + (jumlahhewan - hewanmati));
+    }
+}
