41187f951ca0b70791d91ce8a8698b2dd184e8ee
Hasanah977 <hasanabakri911@gmail.com>
2024-10-08 19:07:11 +0700

    Add files via upload

---
+++ Days37.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days37 {
+    public static void main(String[] args) {
+        double h = 2.5;  // mengisi h dengan nilai 2.5
+           h += 1.2;
+           h -= 2.1;
+           System.out.println("sisa nilai h: " + h);
+    }
+    
+}
