f2db0723e53894319a18bd58bbf514ff40f77997
Hasanah977 <hasanabakri911@gmail.com>
2024-10-09 18:38:00 +0700

    Add files via upload

---
+++ Days38.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days38 {
+    public static void main(String[] args) {
+        int s = 25;
+        s *= 5;  
+        s /= 4;  
+        System.out.println("jumlah hasil s: " + s);
+    }
+}
+
+ 
