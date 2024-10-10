676f934682a1aa6b190ffccacf6e8c91df19a277
Hasanah977 <hasanabakri911@gmail.com>
2024-10-10 07:26:44 +0700

    Add files via upload

---
+++ Days39.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days39 {
+    public static void main(String[] args) {
+        long n = 234567L;
+              n %= 234;
+              System.out.println("sisa jumlah n" + n);
+    }
+    
+}
