2eeed7e1565e674dd6330cd503377c68cc4832b1
Hasanah977 <hasanabakri911@gmail.com>
2024-10-11 16:15:37 +0700

    Add files via upload

---
+++ Days40.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days40 {
+    public static void main(String[] args) {
+        int c = 5;
+        int d = 8;
+        System.out.println("apakah 5 lebih besar dari 8? " + (c > d));        
+        
+        double e = 3.2;
+        double f = 3.0;
+        System.out.println("apakah 3.0 lebih kecil dari 3.2? " + (3.0 < 3.2));
+    }
+    
+}
