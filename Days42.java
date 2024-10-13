57c96d5fdc03f3de5d06e26f90c0a94db763c4d7
Hasanah977 <hasanabakri911@gmail.com>
2024-10-13 17:47:03 +0700

    Add files via upload

---
+++ Days42.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days42 {
+    public static void main(String[] args) {
+        int ada = 25;
+        double tidakada = 2.5;
+       
+        boolean samadengan = ( ada == 25 );
+        boolean tidaksamadengan = (tidakada != 2.5 );
+        
+        System.out.println(" Apakah ada sama dengan 25?  " + samadengan );
+        System.out.println("Apakah tidakada tidak sama dengan 2.5? " + tidaksamadengan);
+    }
+    
+}
