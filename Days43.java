b0c478f57ed09ce0b737e976e7342f57b7751ce3
Hasanah977 <hasanabakri911@gmail.com>
2024-10-14 18:04:38 +0700

    Add files via upload

---
+++ Days43.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days43 {
+    public static void main(String[] args) {
+        
+        double x = 2.5;
+        int z = 25;
+        
+        System.out.println("apakah x lebih kecil dari z?: " + ( x <= z));
+        
+        float n = 20.06f;
+        long m = 2006;
+        
+        System.out.println("apakah n lebih besar dari m?: " + (n >= m));
+    }
+}
