133dd7fc132bd9a9f5c842253efa3ed984d951b4
Hasanah977 <hasanabakri911@gmail.com>
2024-10-15 16:51:52 +0700

    Add files via upload

---
+++ Days44.java
@@ -0,0 +1,23 @@
+
+import java.math.BigInteger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days44 {
+    public static void main(String[] args) {
+        
+        long hp = 289987654l;
+        Integer balik = 9982;
+        
+        System.out.println(hp <  289987654 && balik > 9982 );
+        System.out.println(hp >  289987654 && balik > 9982 );
+    }
+    
+}
