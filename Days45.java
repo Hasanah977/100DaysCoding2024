5d4a031c73a2b402cd64a948aa0287e911675800
Hasanah977 <hasanabakri911@gmail.com>
2024-10-16 19:08:26 +0800

    Add files via upload

---
+++ Days45.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days45 {
+    public static void main(String[] args) {
+        
+        int ha = 20;
+        int sa = 25;
+   
+        System.out.println( ha >  20 ||  sa > 25);
+        System.out.println( ha >=  20 ||  sa >= 25);
+    }
+}
