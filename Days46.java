5b83b10c7d5e9b6c6267e7466a31412ca82a0720
Hasanah977 <hasanabakri911@gmail.com>
2024-10-17 20:26:54 +0800

    Add files via upload

---
+++ Days46.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days46 {
+    public static void main(String[] args) {
+        String UTS = "true";
+        Boolean isUTShigh = Boolean.parseBoolean(UTS);
+        System.out.println("nilai UTS saya tinggi " + !isUTShigh);
+    }
+    
+}
