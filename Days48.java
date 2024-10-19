86a539ec5600a2c996d620427251665fe1457960
Hasanah977 <hasanabakri911@gmail.com>
2024-10-19 18:51:31 +0800

    Add files via upload

---
+++ Days48.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days48 {
+    public static void main(String[] args) {
+        int score = 74;
+        if (score >= 75)  {
+        System.out.println("selamat anda keterima di ITB");
+    } else{
+        System.out.println("maaf anda gagal");
+    }
+    
+}
+}
