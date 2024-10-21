04ecfa02276936c473a9581a6478af29ee1cbc30
Hasanah977 <hasanabakri911@gmail.com>
2024-10-21 17:54:10 +0800

    Add files via upload

---
+++ Days50.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days50 {
+     public static void main(String[] args) {
+        int score = 40;
+        if (score >= 91)  {
+        System.out.println("rangking 1");
+        
+    } else if (score >= 81 ){      
+        System.out.println("rangking 2l");
+        
+    } else if (score >= 71){
+               System.out.println(" rangking 3");
+    } else{
+            System.out.println("tidak rangking");
+    }
+    
+}
+}
+
+
