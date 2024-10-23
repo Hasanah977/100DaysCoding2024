503f63234e7d23df534ea5a9c059a317126286bb
Hasanah977 <hasanabakri911@gmail.com>
2024-10-23 20:48:06 +0800

    Add files via upload

---
+++ Days52.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days52 {
+    public static void main(String[] args) {
+        int umur =10;
+                String aturan = umur>= 18 ? "Bisa bawah motor" : "Tidak bisa bawah motor";
+                System.out.println(aturan);
+    }
+    
+}
