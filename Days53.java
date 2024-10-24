d9c4eacbfca6496bcf449378137cede6c0f9c9d0
Hasanah977 <hasanabakri911@gmail.com>
2024-10-24 19:17:47 +0700

    Add files via upload

---
+++ Days53.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Days53 {
+    public static void main(String[] args) {
+        String[] masadepan = { "indonesia", "menuju","generasi", "cemas" };
+        for (String genz : masadepan) {
+        System.out.println(genz);
+    }
+    }
+}
