919fb17d2de42c673b545240b75360728ac1c6ef
Hasanah977 <hasanabakri911@gmail.com>
2024-10-25 18:50:19 +0700

    Add files via upload

---
+++ Days54.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+
+
+public class Days54 {
+    public static void main(String[] args) {
+        
+   for (int jumlah = 0; jumlah < 5; jumlah++) {
+     System.out.println(jumlah);
+  
+}
+    }
+    }
+    
