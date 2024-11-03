2231b825c668c620f3b7b804e68cb6ab386c5817
Hasanah977 <hasanabakri911@gmail.com>
2024-11-03 20:14:38 +0700

    Add files via upload

---
+++ Days63.java
@@ -0,0 +1,13 @@
+
+public class Days63 {
+    public static void main(String[] args) {
+        for ( int a = 2;a<= 20 ;a++){
+            
+            if( a % 4 == 2){
+                continue;
+                
+            }
+            System.out.println(a);
+        }
+    }
+}
