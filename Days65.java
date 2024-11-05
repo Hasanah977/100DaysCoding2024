1f3142aeb14ecc8d9336bae7d1bc2b6a6ab73988
Hasanah977 <hasanabakri911@gmail.com>
2024-11-05 20:25:07 +0700

    Add files via upload

---
+++ Days65.java
@@ -0,0 +1,10 @@
+
+public class Days65 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 4; a++) {
+            for (int b = 1; b <= 4; b++) {
+                System.out.println("loop luar a : " + a + ", loop dalam b : " + b);
+            }
+        }
+    }
+}
