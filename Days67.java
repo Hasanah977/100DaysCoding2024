64e35a7919843426630a31072cb1925243b0cb9f
Hasanah977 <hasanabakri911@gmail.com>
2024-11-07 18:57:17 +0700

    Add files via upload

---
+++ Days67.java
@@ -0,0 +1,11 @@
+public class Days67 {
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    // Metode yang menggunakan void sebagai tipe pengembalian
+    public static void printHello() {
+        System.out.println("Hello!");
+    }
+}
+
