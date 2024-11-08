a04567ca80fdbf631494d6e51efffd490b79818c
Hasanah977 <hasanabakri911@gmail.com>
2024-11-08 17:26:46 +0700

    Add files via upload

---
+++ Days68.java
@@ -0,0 +1,12 @@
+public class Days68 {
+
+    public int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Days68 days = new Days68(); 
+        int hasil = days.tambah(5, 10);
+        System.out.println(hasil); 
+    }
+}
