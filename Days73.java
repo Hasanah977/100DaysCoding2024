202cfecb40b74dc05623dc1a8aca0966abfed001
Hasanah977 <hasanabakri911@gmail.com>
2024-11-13 21:20:25 +0700

    Add files via upload

---
+++ Days73.java
@@ -0,0 +1,11 @@
+
+public class Days73 {
+
+    public static void main(String[] args) {
+        String nama = "Prezekc";
+
+        char inisial = nama.charAt(0);
+
+        System.out.println("Inisial " + inisial);
+    }
+}
