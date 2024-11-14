a3cdc28570f9422b30dea75902f60525cef54602
Hasanah977 <hasanabakri911@gmail.com>
2024-11-14 21:45:42 +0700

    Add files via upload

---
+++ Days74.java
@@ -0,0 +1,9 @@
+
+public class Days74 {
+    public static void main(String[] args) {
+        String hobby = "game";
+
+        int panjang = hobby.length();
+        System.out.println("Panjang kalimat : " + panjang);
+    }
+}
