803121dcce0e2324489fbbe26127e257cbbc7ac2
Hasanah977 <hasanabakri911@gmail.com>
2024-11-15 21:10:52 +0700

    Add files via upload

---
+++ Days75.java
@@ -0,0 +1,8 @@
+
+public class Days75 {
+    public static void main(String[] args) {
+        String teks = "PreZekC";
+        String hasil = teks.toLowerCase();
+        System.out.println(hasil); // Output: prezekc
+    }
+}
