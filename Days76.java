510198a7ee558bffee95d149b29f46b53bb6054c
Hasanah977 <hasanabakri911@gmail.com>
2024-11-16 21:03:09 +0700

    Add files via upload

---
+++ Days76.java
@@ -0,0 +1,9 @@
+
+public class Days76 {
+    public static void main(String[] args) {
+        String text = "prezekc";
+        String upperCaseText = text.toUpperCase();
+        System.out.println("Original String: " + text);           // Output: prezekc
+        System.out.println("Uppercase String: " + upperCaseText); // Output: PREZEKC
+    }
+}
