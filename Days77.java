e72bf6ef43e7713f2f43ff34976171f7327ad76f
Hasanah977 <hasanabakri911@gmail.com>
2024-11-17 21:21:23 +0700

    Add files via upload

---
+++ Days77.java
@@ -0,0 +1,11 @@
+
+public class Days77 {
+    public static void main(String[] args) {
+        String a = "jogja";
+        String b = "bandung";
+        String c = "jogja";
+
+        System.out.println("apakah a sama dengan b : " + a.equals(b));
+        System.out.println("apakah a sama dengan c : " + a.equals(c));
+    }
+}
