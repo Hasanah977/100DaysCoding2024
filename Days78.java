d000a4dfe20c2fa4c66d814740cf021bc28037e2
Hasanah977 <hasanabakri911@gmail.com>
2024-11-18 20:56:45 +0700

    Add files via upload

---
+++ Days78.java
@@ -0,0 +1,10 @@
+public class Days78 {
+   public static void main(String[] args) {
+        String str1 = "Jefri";
+        String str2 = "jefri";
+        String str3 = "fateh";
+
+        System.out.println(str1.equalsIgnoreCase(str2)); 
+        System.out.println(str1.equalsIgnoreCase(str3));  
+    }
+}
