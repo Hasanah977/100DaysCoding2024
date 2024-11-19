e0c2f4d357d679a4448896d5cf4fe0dccab2e362
Hasanah977 <hasanabakri911@gmail.com>
2024-11-19 20:42:05 +0700

    Add files via upload

---
+++ Days79.java
@@ -0,0 +1,10 @@
+
+public class Days79 {
+    public static void main(String[] args) {
+        String a = " Pergi ";
+
+        String hasil = a.toString();
+
+        System.out.println("hasil : " + hasil);
+    }
+    }
