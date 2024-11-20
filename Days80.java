205ffb48fce0004ab05bd91ee0e44e90ca370e73
Hasanah977 <hasanabakri911@gmail.com>
2024-11-20 20:14:03 +0700

    Add files via upload

---
+++ Days80.java
@@ -0,0 +1,9 @@
+
+public class Days80 {
+    public static void main(String[] args) {
+        String nama = "  Prezekc  ";
+        System.out.println("Sebelum trim : " + nama);
+        String hasil = nama.trim();
+        System.out.println("Setelah trim : " + hasil);
+    }
+}
