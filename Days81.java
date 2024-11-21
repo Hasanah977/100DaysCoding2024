93b800fe91499c32b493f4d6615b833ebc69d4da
Hasanah977 <hasanabakri911@gmail.com>
2024-11-21 21:00:22 +0700

    Add files via upload

---
+++ Days81.java
@@ -0,0 +1,15 @@
+
+public class Days81 {
+    public static void main(String[] args) {
+        int rangka = 456;
+        double pecahan = 25.2;
+       
+        
+        String Angka=String.valueOf(rangka);
+        String Pecahan = String.valueOf(pecahan);
+        
+        System.out.println("String dari int ="+ Angka);
+        System.out.println("String dari int ="+ Pecahan);
+      
+    }
+}
