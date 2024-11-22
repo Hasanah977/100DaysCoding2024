280e1a2b9b7649bf2fa1ebd56fc4ad91f89dc940
Hasanah977 <hasanabakri911@gmail.com>
2024-11-22 17:57:49 +0700

    Add files via upload

---
+++ Days82.java
@@ -0,0 +1,9 @@
+public class Days82 {
+public static void main(String[] args) {
+    double a = 25.2;
+
+    System.out.println("Ceil: " + Math.ceil(a));
+    System.out.println("Floor: " + Math.floor(a));
+    System.out.println("Round: " + Math.round(a));
+}
+}
