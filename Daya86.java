8e7ebe2a451a4bedd6854600674e40c39b218e51
Hasanah977 <hasanabakri911@gmail.com>
2024-11-26 22:06:40 +0700

    Add files via upload

---
+++ Daya86.java
@@ -0,0 +1,9 @@
+public class Daya86 {
+    public static void main(String args[]) {
+        int[] array = {10, 20, 30, 40, 50}; // Tambahkan ; di akhir baris ini
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + array[i]);
+        }
+    }
+}
---
+++ Days85.java
@@ -0,0 +1,12 @@
+
+public class Days85 {
+    public static void main(String[] args) {
+        // Inisialisasi array dengan nilai langsung
+        int[] nomor = {15, 16, 17, 18, 19};
+        
+        // Cetak elemen array
+        for (int number : nomor) {
+            System.out.println(number);
+        }
+    }
+}
